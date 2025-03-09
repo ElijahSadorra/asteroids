db914a2b47711b372a264c83f0ae8d6331ce439e
ElijahSadorra <elijah.sadorra@gmail.com>
2025-03-09 21:36:34 +0000

    Added player and player movement

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,21 +9,35 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    dt = 0
     running = True
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
     while running:
+
+        # user pressed cross
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+
         screen.fill("black")
 
-        pygame.display.flip()
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
 
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     pygame.quit()
-
-
 
 if __name__ == "__main__":
     main()
